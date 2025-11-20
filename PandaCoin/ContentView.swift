344c7fd7e8b831ee1390f428c5c39e4aef7b895f
BigCode <zuokunde@gmail.com>
2025-11-20 21:02:14 +0800

    Initial Commit

---
+++ PandaCoin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PandaCoin
+//
+//  Created by kevin on 2025/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PandaCoin/PandaCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandaCoinApp.swift
+//  PandaCoin
+//
+//  Created by kevin on 2025/11/20.
+//
+
+import SwiftUI
+
+@main
+struct PandaCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
